da42458813c36986b575f06a246904c0dfe9ffaf
Tauhid <tauhidahmed716@gmail.com>
2024-10-03 21:51:53 +0600

    arrow-long icon changes

--- components/icons.tsx
+++ components/icons.tsx
@@ -340,15 +340,16 @@ export function Pause({ ...props }) {
 
 export function ArrowLong({ ...props }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="24px"
-      viewBox="0 -960 960 960"
-      width="24px"
-      fill="#e8eaed"
-      {...props}
-    >
-      <path d="M700-300l-57-56 84-84H120v-80h607l-83-84 57-56 179 180-180 180z" />
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <g fill="none">
+        <path
+          d="M17.25 8.25L21 12M21 12L17.25 15.75M21 12H3"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </g>
     </svg>
   )
 }
