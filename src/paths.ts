e876c31ddce1a3d2798f861995f046fc45932fd8
Tauhid <tauhidahmed716@gmail.com>
2024-12-07 13:54:47 +0600

    feat: add all route paths in a single file

---
+++ src/paths.ts
@@ -0,0 +1,24 @@
+export const home = () => "/";
+
+export const aboutUs = () => "/";
+
+export const ourBusinesses = () => "/our-businesses";
+export const businessTechnologies = () => "/our-businesses/technologies";
+export const businessMedicalServices = () => "/our-businesses/";
+export const businessLaunderettes = () => "/our-businesses/launderettes";
+export const businessIceIvoire = () => "/our-businesses/ice-ivoire";
+export const businessCommodities = () => "/our-businesses/commodities";
+export const businessRealEstate = () => "/our-businesses/real-estate";
+export const businessFertilizer = () => "/our-businesses/fertilizer";
+export const businessLogistics = () => "/our-businesses/logistics";
+export const businessPetrochemicals = () => "/our-businesses/petrochemicals";
+
+export const sustainability = () => "/";
+
+export const centurionFoundation = () => "/";
+
+export const media = () => "/";
+
+export const career = () => "/";
+
+export const contact = () => "/";
