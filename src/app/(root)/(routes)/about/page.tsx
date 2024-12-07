4d1f26bea5e88bacd17e7a454a986ad0c135055a
Tauhid <tauhidahmed716@gmail.com>
2024-12-07 12:15:45 +0600

    chore(migration): src dir migration
    - for better file management
    - prev it was app directory because of it it becomes messay for large project

--- src/app/(root)/(routes)/about/page.tsx
+++ src/app/(root)/(routes)/about/page.tsx
@@ -9,9 +9,9 @@ import { aboutCards } from "@/data/about-card";
 
 import Wrapper from "@/components/layout/wrapper";
 import BannerAnimation from "@/components/shared/banner-animation";
-import aboutBannerImage from "@/public/assets/images/about/about-team.webp";
-import ceoImage from "@/public/assets/images/about/ceo.webp";
-import executiveImage from "@/public/assets/images/about/executive.webp";
+import aboutBannerImage from "/public/assets/images/about/about-team.webp";
+import ceoImage from "/public/assets/images/about/ceo.webp";
+import executiveImage from "/public/assets/images/about/executive.webp";
 import Button from "@/components/shared/button";
 import SectionTitle from "@/components/shared/section-title";
 
@@ -28,12 +28,14 @@ export default function Page() {
         <Wrapper className="relative text-white font-semibold z-10">
           <div className="max-w-[65ch] rounded">
             <h1 className="">About Us</h1>
-            <h2 className="text-4.5xl mt-2 leading-none">Premier Service Provider</h2>
+            <h2 className="text-4.5xl mt-2 leading-none">
+              Premier Service Provider
+            </h2>
             <p className="text-md leading-tight mt-4">
-              Providing Your Basic Needs With our transformational projects and businesses
-              across food, shelter and construction, Centurion Group continues to grow its
-              vision of becoming the leading provider of essential services and needs in
-              Africa.
+              Providing Your Basic Needs With our transformational projects and
+              businesses across food, shelter and construction, Centurion Group
+              continues to grow its vision of becoming the leading provider of
+              essential services and needs in Africa.
             </p>
           </div>
         </Wrapper>
@@ -45,12 +47,13 @@ export default function Page() {
           <div className="text-center">
             <SectionTitle>Our Mission & Vision</SectionTitle>
             <p className="mt-4 px-6 text-left lg:text-center">
-              Centurion Group continues to grow its focus of becoming the leading provider
-              of essential needs in Services, Technologies, Food and Shelter in
-              Sub-Saharan Africa. Our vision is to be a world-class enterprise that is
-              passionate about the quality of life of the general populace. Our mission is
-              to touch the lives of people by providing their basic needs. We continue to
-              sustain and improve on our market leadership in Technologies and medical
+              Centurion Group continues to grow its focus of becoming the
+              leading provider of essential needs in Services, Technologies,
+              Food and Shelter in Sub-Saharan Africa. Our vision is to be a
+              world-class enterprise that is passionate about the quality of
+              life of the general populace. Our mission is to touch the lives of
+              people by providing their basic needs. We continue to sustain and
+              improve on our market leadership in Technologies and medical
               services, Commodities, Self-service launderettes.
             </p>
           </div>
@@ -66,15 +69,18 @@ export default function Page() {
         <Wrapper>
           <div className="bg-transparent ml-auto max-w-[60ch] p-10 relative z-10 rounded backdrop-blur-3xl mb-20 md:mb-0">
             <div className="text-white w-full relative">
-              <h2 className="text-2xl md:text-3xl lg:text-4xl">CEO Biography</h2>
+              <h2 className="text-2xl md:text-3xl lg:text-4xl">
+                CEO Biography
+              </h2>
               <div className="mt-6">
                 <p className="text-sm">
-                  Mr Bandeira is the CEO of Centurion Group, leading the organization with
-                  a vision for growth and innovation. With years of experience in the
-                  industry, He has successfully driven strategic initiatives, ensuring
-                  sustainable development and market leadership. Under his leadership,
-                  Centurion Group has achieved significant milestones and continues to
-                  pioneer new advancements.
+                  Mr Bandeira is the CEO of Centurion Group, leading the
+                  organization with a vision for growth and innovation. With
+                  years of experience in the industry, He has successfully
+                  driven strategic initiatives, ensuring sustainable development
+                  and market leadership. Under his leadership, Centurion Group
+                  has achieved significant milestones and continues to pioneer
+                  new advancements.
                 </p>
                 {/* <Button className="mt-4">About More</Button> */}
               </div>
@@ -88,9 +94,9 @@ export default function Page() {
           <div className="text-center">
             <SectionTitle>Centurion group leadership</SectionTitle>
             <p className="mt-4 px-6">
-              Our senior executives bring tremendous experience, visionary thinking and a
-              shared commitment to excellence, creativity, and innovation to the day to
-              day operation of the company.
+              Our senior executives bring tremendous experience, visionary
+              thinking and a shared commitment to excellence, creativity, and
+              innovation to the day to day operation of the company.
             </p>
           </div>
         </Wrapper>
@@ -197,10 +203,10 @@ export default function Page() {
             Strategic Tech Partner for Productivity and Growth
           </SectionTitle>
           <p className="max-w-[80ch]">
-            We are a company that is at the forefront of technology, becoming a strategic
-            ally for your company or business, providing you with the best solutions and
-            services that ensure your productivity, competitiveness, permanence and
-            excellent service for your clients.
+            We are a company that is at the forefront of technology, becoming a
+            strategic ally for your company or business, providing you with the
+            best solutions and services that ensure your productivity,
+            competitiveness, permanence and excellent service for your clients.
           </p>
 
           {/* <div className="max-w-[65ch] rounded bg-transparent backdrop-blur-sm">
@@ -222,8 +228,8 @@ export default function Page() {
         <div className="text-center">
           <SectionTitle>OUR VALUES</SectionTitle>
           <p className="mt-4 px-6">
-            Our values are the foundation of our company and define who we are and how we
-            work.
+            Our values are the foundation of our company and define who we are
+            and how we work.
           </p>
         </div>
 
@@ -233,7 +239,8 @@ export default function Page() {
               key={card.title}
               className={`flex flex-col gap-10 items-center ${
                 index % 2 === 1 ? "md:flex-row-reverse" : "md:flex-row"
-              }`}>
+              }`}
+            >
               <div className="md:flex-1 max-w-[60ch] space-y-6">
                 <h3 className="text-3xl md:text-4xl font-bold">{card.title}</h3>
                 <p>{card.description}</p>
