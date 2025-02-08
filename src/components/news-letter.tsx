e21c1aa7bacc654ed0f0ac1c85e86f6a22fae12c
= <=>
2025-02-08 10:32:13 +0600

    spacing and sizing fix

--- src/components/news-letter.tsx
+++ src/components/news-letter.tsx
@@ -9,7 +9,7 @@ import { useTranslations } from "next-intl";
 export default function NewsLetter() {
   const text = useTranslations("text");
   return (
-    <section className="h-60 md:h-84 relative flex items-center justify-center text-center">
+    <section className="h-72 md:h-96 relative flex items-center justify-center text-center">
       <div className="h-full w-full relative">
         <Image
           fill
@@ -23,13 +23,19 @@ export default function NewsLetter() {
           Get started to up your business <br /> with personal AI manager
         </Heading>
         <form className="mt-6">
-          <div className="space-x-4">
+          <div className="flex flex-col sm:flex-row gap-4">
             <input
               placeholder="Enter your email"
               type="text"
-              className="rounded-full h-12 px-4 w-60 lg:w-96 text-white bg-transparent border border-gray-500"
+              className="rounded-full h-12 px-4 w-full sm:w-96 text-white bg-transparent border border-gray-500"
             />
-            <Button asChild variant="primary" shape="pill" size="md">
+            <Button
+              asChild
+              variant="primary"
+              shape="pill"
+              size="lg"
+              className="md:h-12"
+            >
               <Link href="#">{text("try_more")}</Link>
             </Button>
           </div>
