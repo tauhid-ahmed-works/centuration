6883b8bdeef5f6f0aa2b135a919d6e57b1118b98
= <=>
2025-02-10 00:11:00 +0600

    add hover option

--- src/components/intregation.tsx
+++ src/components/intregation.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Integration() {
   return (
     <div className="fixed right-4 bottom-4">
-      <div className="inline-flex flex-col gap-3 bg-white rounded-full p-2 shadow">
+      <div className="inline-flex group flex-col gap-3 bg-white rounded-full p-2 shadow">
         <button className="cursor-pointer flex justify-center items-center size-6 md:size-10 lg:size-16 rounded-full border">
           <Image
             src="/assets/icons/arrow-up.svg"
@@ -21,7 +21,7 @@ export function Integration() {
             alt="whatsapp"
             className="size-1/2"
           />
-          <span className="absolute px-4 py-2 bg-white drop-shadow-md whitespace-nowrap rounded right-full mr-7 font-semibold after:border-[8px] after:border-r-transparent after:border-t-transparent after:border-b-transparent after:absolute after:left-full after:top-1/2 after:-translate-y-1/2 after:border-white hidden lg:block">
+          <span className="absolute px-4 py-2 bg-white drop-shadow-md whitespace-nowrap rounded right-full mr-7 font-semibold after:border-[8px] after:border-r-transparent after:border-t-transparent after:border-b-transparent after:absolute after:left-full after:top-1/2 after:-translate-y-1/2 after:border-white hidden group-hover:block">
             Contact Us
           </span>
         </button>
