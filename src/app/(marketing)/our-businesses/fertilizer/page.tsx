f3104ed83c24f94fab17f194f64e5377c4150dc3
= <=>
2025-02-28 16:20:20 +0600

    feat(business-information): add business information to pages

--- src/app/(marketing)/our-businesses/fertilizer/page.tsx
+++ src/app/(marketing)/our-businesses/fertilizer/page.tsx
@@ -1,38 +1,42 @@
-import { Banner, BannerType } from "@/components/banner";
+import { BannerType, Banner } from "@/components/banner";
 import { useIntl } from "@/lib/get-intl";
+import PageInformation from "@/components/page-information";
 
-export default function LogisticsPage() {
-  const commoditiesData = useIntl("business_fertilizer");
-  const bannerIntl = commoditiesData("banner");
-  const bannerData: BannerType[] = [
-    {
-      mediaSrc:
-        "/assets/images/business/fertilizer/banner/banner-image-01.webp",
-      mediaType: "image",
-      title: bannerIntl[1].title,
-      content: bannerIntl[1].content,
-      learn_more: "/",
-    },
-    {
-      mediaSrc:
-        "/assets/images/business/fertilizer/banner/banner-image-02.webp",
-      mediaType: "image",
-      title: bannerIntl[2].title,
-      content: bannerIntl[2].content,
-      learn_more: "/",
-    },
-    {
-      mediaSrc:
-        "/assets/images/business/fertilizer/banner/banner-image-03.webp",
-      mediaType: "image",
-      title: bannerIntl[3].title,
-      content: bannerIntl[3].content,
-      learn_more: "/",
-    },
-  ];
+const bannerMedia = [
+  {
+    mediaSrc: "/assets/images/business/fertilizer/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+  {
+    mediaSrc: "/assets/images/business/fertilizer/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+  {
+    mediaSrc: "/assets/images/business/fertilizer/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+];
+
+export default function PetrochemicalsPage() {
+  const petrochemicalsData = useIntl("business_fertilizer");
+  const bannerIntl = petrochemicalsData("banner");
+  const bannerData: BannerType[] = bannerIntl.map(
+    (item: Record<string, string>, index: number) => ({
+      ...item,
+      ...bannerMedia[index],
+    })
+  );
+  const information = petrochemicalsData("information");
   return (
     <>
       <Banner data={bannerData} />
+      <PageInformation
+        title={information.title}
+        content={information.content}
+      />
     </>
   );
 }
--- src/app/(marketing)/our-businesses/petrochemicals/page.tsx
+++ src/app/(marketing)/our-businesses/petrochemicals/page.tsx
@@ -1,38 +1,45 @@
 import { BannerType, Banner } from "@/components/banner";
 import { useIntl } from "@/lib/get-intl";
+import PageInformation from "@/components/page-information";
+
+const bannerMedia = [
+  {
+    mediaSrc:
+      "/assets/images/business/petrochemicals/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+  {
+    mediaSrc:
+      "/assets/images/business/petrochemicals/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+  {
+    mediaSrc:
+      "/assets/images/business/petrochemicals/banner/banner-image-01.webp",
+    mediaType: "image",
+    learn_more: "/",
+  },
+];
 
 export default function PetrochemicalsPage() {
-  const commoditiesData = useIntl("business_petrochemicals");
-  const bannerIntl = commoditiesData("banner");
-  const bannerData: BannerType[] = [
-    {
-      mediaSrc:
-        "/assets/images/business/petrochemicals/banner/banner-image-01.webp",
-      mediaType: "image",
-      title: bannerIntl[1].title,
-      content: bannerIntl[1].content,
-      learn_more: "/",
-    },
-    {
-      mediaSrc:
-        "/assets/images/business/petrochemicals/banner/banner-image-02.webp",
-      mediaType: "image",
-      title: bannerIntl[2].title,
-      content: bannerIntl[2].content,
-      learn_more: "/",
-    },
-    {
-      mediaSrc:
-        "/assets/images/business/petrochemicals/banner/banner-image-03.webp",
-      mediaType: "image",
-      title: bannerIntl[3].title,
-      content: bannerIntl[3].content,
-      learn_more: "/",
-    },
-  ];
+  const petrochemicalsData = useIntl("business_petrochemicals");
+  const bannerIntl = petrochemicalsData("banner");
+  const bannerData: BannerType[] = bannerIntl.map(
+    (item: Record<string, string>, index: number) => ({
+      ...item,
+      ...bannerMedia[index],
+    })
+  );
+  const information = petrochemicalsData("information");
   return (
     <>
       <Banner data={bannerData} />
+      <PageInformation
+        title={information.title}
+        content={information.content}
+      />
     </>
   );
 }
