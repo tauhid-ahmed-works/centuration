5b36881de4dc6512e5121c86e3f6aec18f78dcdc
= <=>
2025-02-28 16:18:15 +0600

    chore(business:locale): add rest business information data

--- src/dictionaries/locales/business-fertilizer.ts
+++ src/dictionaries/locales/business-fertilizer.ts
@@ -1,39 +1,49 @@
 export const fertilizerEn = {
-  banner: {
-    1: {
+  banner: [
+    {
       title: "High-Quality Fertilizers for Agricultural Growth",
       content:
         "Centurion provides premium fertilizers that enhance crop productivity and contribute to sustainable agricultural practices worldwide.",
     },
-    2: {
+    {
       title: "Innovative Solutions for Sustainable Farming",
       content:
         "Our fertilizers are designed to support sustainable farming, improving soil health and crop yields while minimizing environmental impact.",
     },
-    3: {
+    {
       title: "Optimizing Crop Yield with Precision Nutrition",
       content:
         "We offer specialized fertilizers tailored to specific crop needs, ensuring optimal growth and maximum yield through precision nutrition.",
     },
+  ],
+  information: {
+    title: "High-Quality Fertilizers for Sustainable Farming",
+    content:
+      "Our fertilizers are designed to enhance soil health, improve crop yield, and promote eco-friendly farming practices. With a range of organic and synthetic options, we support farmers in achieving long-term agricultural success and food security.",
   },
 };
 
 export const fertilizerFr = {
-  banner: {
-    1: {
+  banner: [
+    {
       title: "Fertilisants de Haute Qualité pour la Croissance Agricole",
       content:
         "Centurion fournit des fertilisants de qualité supérieure qui augmentent la productivité des cultures et contribuent à des pratiques agricoles durables dans le monde entier.",
     },
-    2: {
+    {
       title: "Solutions Innovantes pour une Agriculture Durable",
       content:
         "Nos fertilisants sont conçus pour soutenir une agriculture durable, améliorant la santé du sol et les rendements des cultures tout en minimisant l'impact environnemental.",
     },
-    3: {
+    {
       title: "Optimiser le Rendement des Cultures avec une Nutrition Précise",
       content:
         "Nous proposons des fertilisants spécialisés adaptés aux besoins spécifiques des cultures, garantissant une croissance optimale et un rendement maximal grâce à une nutrition précise.",
     },
+  ],
+  information: {
+    title: "Engrais de Haute Qualité pour une Agriculture Durable",
+    content:
+      "Nos engrais sont conçus pour améliorer la santé des sols, augmenter les rendements agricoles et promouvoir des pratiques agricoles respectueuses de l’environnement. Grâce à une gamme de solutions organiques et synthétiques, nous aidons les agriculteurs à garantir le succès agricole à long terme et la sécurité alimentaire.",
   },
 };
